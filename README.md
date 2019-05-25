a910e717fdab59d393c2d79b3a090765d62258d5
Noel Wilson <noel.wilson@supercarers.com>
2019-05-25 21:33:25 +0100

    Updating script to work with python 3 and adding notes to README about how it works

--- README.md
+++ README.md
@@ -6,6 +6,31 @@ https://gist.github.com/ziplus4/3bf8cc14541a16c65206
 
 Looking at horizontal scaling of database data across multiple db nodes
 
+## How this code works:
+
+This snippet will setup 6 database "nodes" which are just sqlite files these could be sustituted for postgres servers. These are setup with binds which is a key map for sqlalchemy for databases.
+
+The Notice model is matched to the global database node in a straight forward fashion. The user and log models have a regex binding pattern, they will randomly go to a different bound database based on the hash value defined in the get_shard_key.
+
+This means we have vertical and horizontal scaling in our sqlalchemy snippet.
+
+## Master slave dbs
+
+Master DBs should be the only databases receiving write queries currently this isn't setup and the regex filters out slave databases althrough this should be modified to only make writes to mater dbs
+
+### get_tables_for_bind()
+
+This is used to add tables for each database, if a `__bind__` regex pattern matches the name of a database that table will be added to the database, so for example:
+
+class User(db.Model):
+    __bind_key__ = db.BindingKeyPattern('[^_]+_user_\d\d')
+
+will be added to :
+    'master_user_01': 'sqlite:///./master_user_01.db',
+    'master_user_02': 'sqlite:///./master_user_02.db',
+    'slave_user': 'sqlite:///./slave_user.db',
+
+
 ## Setup
 
 To setup python 3 environment for project first install direnv and pyenv on your machine:
--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import re
 from flask import Flask
 
 from flask_sqlalchemy import SQLAlchemy as BaseSQLAlchemy
-from flask_sqlalchemy import _SignallingSession as BaseSignallingSession
-from flask_sqlalchemy import orm, partial, get_state
+from flask_sqlalchemy import SignallingSession as BaseSignallingSession
+from flask_sqlalchemy import orm, get_state
+import sqlalchemy.orm as orm
+from functools import partial
 
 from datetime import datetime
 
@@ -25,7 +27,7 @@ class _BindingKeyPattern(object):
 
     def get_shard_key(self, hash_num):
         if self._shard_keys is None:
-            self._shard_keys = [key for key, value in self.db.app.config['SQLALCHEMY_BINDS'].iteritems() if self.compiled_pattern.match(key)]
+            self._shard_keys = [key for key, value in self.db.app.config['SQLALCHEMY_BINDS'].items() if self.compiled_pattern.match(key)]
             self._shard_keys.sort()
 
         return self._shard_keys[hash_num % len(self._shard_keys)]
@@ -66,24 +68,24 @@ class _SignallingSession(BaseSignallingSession):
             return state.db.get_engine(self.app, bind=binding_key)
 
     def __find_binding_key(self, mapper):
-        if mapper is None: # 맵퍼 없음
+        if mapper is None:
             return self._binding_key
         else:
             mapper_info = getattr(mapper.mapped_table, 'info', {})
             mapped_binding_key = mapper_info.get('bind_key')
-            if mapped_binding_key: # 맵핑된 바인딩 키 존재
-                if type(mapped_binding_key) is str: # 정적 바인딩
+            if mapped_binding_key:
+                if type(mapped_binding_key) is str:
                     return mapped_binding_key
-                else: # 동적 바인딩
-                    if mapped_binding_key.match(self._binding_key): # 현재 바인딩
+                else:
+                    if mapped_binding_key.match(self._binding_key):
                         return self._binding_key
-                    else: # 푸쉬된 바인딩
+                    else:
                         for pushed_binding_key in reversed(self._binding_keys):
                             if pushed_binding_key and mapped_binding_key.match(pushed_binding_key):
                                 return pushed_binding_key
                         else:
                             raise Exception('NOT_FOUND_MAPPED_BINDING:%s CURRENT_BINDING:%s PUSHED_BINDINGS:%s' % (repr(mapped_binding_key), repr(self._binding_key), repr(self._binding_keys[1:])))
-            else: # 맵핑된 바인딩 키가 없으면 디폴트 바인딩
+            else:
                 return self._binding_key
 
 
@@ -113,7 +115,7 @@ class SQLAlchemy(BaseSQLAlchemy):
 
     def get_tables_for_bind(self, bind=None):
         result = []
-        for table in self.Model.metadata.tables.itervalues():
+        for table in self.Model.metadata.tables.values():
             table_bind_key = table.info.get('bind_key')
             if table_bind_key == bind:
                 result.append(table)
@@ -130,6 +132,7 @@ class SQLAlchemy(BaseSQLAlchemy):
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
+
 class Notice(db.Model):
     __bind_key__ = 'global'
 
@@ -139,6 +142,7 @@ class Notice(db.Model):
 
     def __repr__(self):
         return "%s<id=%d,msg='%s'>" % (self.__class__.__name__, self.id, self.msg)
+
 
 class User(db.Model):
     __bind_key__ = db.BindingKeyPattern('[^_]+_user_\d\d')
@@ -154,6 +158,7 @@ class User(db.Model):
     @classmethod
     def get_shard_key(cls, nickname):
         return cls.__bind_key__.get_shard_key(hash(nickname))
+
 
 class LoginLog(db.Model):
     __bind_key__ = db.BindingKeyPattern('[^_]+_log')
@@ -173,7 +178,7 @@ if __name__ == '__main__':
         'master_log':  'sqlite:///./master_log.db', 
         'slave_log':  'sqlite:///./slave_log.db', 
     }
-        
+    
     db.drop_all()
     db.create_all()
 
